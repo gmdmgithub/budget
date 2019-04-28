3610905124411c76bc3c3cdd7a27b1d132fe6269
gmdmgithub <gmika@interia.pl>
2019-04-28 13:45:31 +0200

    code wiring statement with institution

--- models/institution.go
+++ models/institution.go
@@ -3,6 +3,7 @@ package models
 // Institution - model for institution like bank, Broker
 type Institution struct {
 	ID      string `json:"_id"`
+	Code    string `json:"code"`
 	Name    string `json:"name"`
 	Comment string `json:"comment"`
 	Type    string `json:"type"`
--- models/statement.go
+++ models/statement.go
@@ -4,16 +4,16 @@ import "time"
 
 // Statement - model for db object that hold investments and loans
 type Statement struct {
-	ID           string    `json:"_id"`
-	Name         string    `json:"name"`
-	Comment      string    `json:"comment"`
-	InstCode     string    `json:"inst_code"`
-	TypeCode     string    `json:"type_code"`
-	StartDate    time.Time `json:"start_date"`
-	EndDate      time.Time `json:"end_date"`
-	InterestRate float64   `json:"interest_rate"`
-	Amount       int       `json:"amount"`
-	CurrencyCode string    `json:"currency_code"`
-	Active       bool      `json:"active"`
+	ID           string    `json:"_id" bson:"_id"`
+	Name         string    `json:"name" bson:"name"`
+	Comment      string    `json:"comment" bson:"comment"`
+	InstCode     string    `json:"inst_code" bson:"inst_code"`
+	TypeCode     string    `json:"type_code" bson:"type_code"`
+	StartDate    time.Time `json:"start_date" bson:"start_date"`
+	EndDate      time.Time `json:"end_date" bson:"end_date"`
+	InterestRate float64   `json:"interest_rate" bson:"interest_rate"`
+	Amount       int       `json:"amount" bson:"amount"`
+	CurrencyCode string    `json:"currency_code" bson:"currency_code"`
+	Active       bool      `json:"active" bson:"active"`
 	Audit
 }
