1f9dcf644230b49268f48b6dd788a6d8878a1a7b
gmdmgithub <gmika@interia.pl>
2019-05-04 00:34:12 +0200

    currency add methos the newest exchangerate for currency change in the future - not follow DRY

--- handler/currency.go
+++ handler/currency.go
@@ -62,20 +62,20 @@ func createCurrency(w http.ResponseWriter, r *http.Request) {
 
 func currencyDate(w http.ResponseWriter, r *http.Request) {
 
-	params := r.URL.Query()
+	log.Printf("URL queries %+v", r.URL.Query())
+
 	curCode := chi.URLParam(r, "curCode")
 	if curCode != "USD" && curCode != "EUR" && curCode != "CHF" && curCode != "GBP" {
 		curCode = "PLN"
 	}
 
-	log.Printf("URL queries? %+v", params)
-
-	filter := bson.M{}
+	// options
 	opts := options.Find()
 	opts.SetLimit(1)
 	// Sort by `date` field +1 ascending -1 descending
 	opts.Sort = bson.M{"date": 1}
 
+	params := r.URL.Query()
 	layout := "2006-01-02"
 	s := params.Get("date")
 	t, err := time.Parse(layout, s)
@@ -85,17 +85,19 @@ func currencyDate(w http.ResponseWriter, r *http.Request) {
 		t = time.Date(t.Year(), t.Month(), t.Day(), 0, 0, 0, 0, t.Location())
 	}
 	tto := time.Now().Add(24 * time.Hour)
+
+	filter := bson.M{}
 	filter["date"] = bson.M{"$gte": t, "$lte": tto}
 	filter["code"] = bson.M{"$eq": curCode}
 
-	log.Printf("filter is: %+v otp %+v", filter, opts)
-
+	// proper db query
 	curs, err := driver.GetCurrencies(filter, opts)
 	if err != nil {
 		log.Printf("Problem with get currencies %v", curs)
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
+	// simplify response - object instead array
 	var cur model.Currency
 	if len(curs) == 1 {
 		cur = curs[0]
@@ -107,7 +109,8 @@ func currencyDate(w http.ResponseWriter, r *http.Request) {
 			Base:         true,
 		}
 	}
-
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("content-type", "application/json")
 	if err := json.NewEncoder(w).Encode(&cur); err != nil {
 		log.Printf("Problem with encoding currencies %v", cur)
 		http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -128,7 +131,8 @@ func currencies(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("content-type", "application/json")
 	if err := json.NewEncoder(w).Encode(cur); err != nil {
 		log.Printf("Problem with encoding currencies %v", cur)
 		http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -138,5 +142,51 @@ func currencies(w http.ResponseWriter, r *http.Request) {
 }
 
 func currency(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hi there one currency here"))
+
+	// similar to date - the nearest previous currency - to be rewrite - first simple implementation - unfortunately not follow DRY
+	curCode := chi.URLParam(r, "curCode")
+	if curCode != "USD" && curCode != "EUR" && curCode != "CHF" && curCode != "GBP" {
+		curCode = "PLN"
+	}
+
+	// options
+	opts := options.Find()
+	opts.SetLimit(1)
+	// Sort by `date` field +1 ascending -1 descending
+	opts.Sort = bson.M{"date": -1}
+
+	tto := time.Now().Add(24 * time.Hour)
+
+	filter := bson.M{}
+	filter["date"] = bson.M{"$lte": tto}
+	filter["code"] = bson.M{"$eq": curCode}
+
+	// proper db query
+	curs, err := driver.GetCurrencies(filter, opts)
+	if err != nil {
+		log.Printf("Problem with get currencies %v", curs)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	// simplify response - object instead array
+	var cur model.Currency
+	if len(curs) == 1 {
+		cur = curs[0]
+	} else {
+		cur = model.Currency{
+			Code:         "PLN",
+			ExchangeRate: 1,
+			Date:         time.Now(),
+			Base:         true,
+		}
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("content-type", "application/json")
+	if err := json.NewEncoder(w).Encode(&cur); err != nil {
+		log.Printf("Problem with encoding currencies %v", cur)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+	// w.Write([]byte("Hi there one currency here"))
 }
